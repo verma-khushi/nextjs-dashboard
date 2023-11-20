93b10c5acfb08b016fbc1e9e14715fdeeb98d63d
Khushi Verma <khushiv@geekyants.com>
2023-11-20 12:36:25 +0530

    Nextjs-dashboard.

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -9,8 +9,11 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     <button
       {...rest}
       className={clsx(
-        'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
-        className,
+        'inline-flex items-center rounded-full px-2 py-1 text-sm',
+        {
+          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-green-500 text-white': status === 'paid',
+        },
       )}
     >
       {children}
